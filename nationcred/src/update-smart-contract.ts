b872b0df7a7c50e8b14121c09baca5ee69e73be8
aahna-ashina <95955389+aahna-ashina@users.noreply.github.com>
2023-03-30 21:13:57 +0800

    feat: get passport ids array from csv (#94)

--- nationcred/src/update-smart-contract.ts
+++ nationcred/src/update-smart-contract.ts
@@ -1,8 +1,50 @@
 require('dotenv').config()
 const NationCred = require('../abis/NationCred.json')
+const fs = require('fs')
+const Papa = require('papaparse')
 import { BigNumber, ethers } from "ethers"
 
+/**
+ * Fetches last week's active citizens from `nationcred-active-citizens.csv`
+ * 
+ * @returns A Promise with a number array, where each number represents a passport ID
+ */
+const lookupActiveCitizensFromCSV = async (): Promise<number[]> => {
+  console.log('lookupActiveCitizensFromCSV')
+
+  const filePath = "output/nationcred-active-citizens.csv"
+  const file: File = fs.readFileSync(filePath)
+  const csvData = file.toString()
+  console.info('csvData:\n', csvData)
+
+  const promise: Promise<number[]> = new Promise((resolve, reject) => {
+    Papa.parse(csvData, {
+      header: true,
+      skipEmptyLines: true,
+      complete: (results: any) => {
+        const data = results.data
+        console.log('data.length:', data.length)
+        
+        const lastWeekEnd = data[data.length - 1]
+        console.log('lastWeekEnd:', lastWeekEnd)
+
+        // Convert from string ("1,2,3") to array of numbers ([ 1, 2, 3 ])
+        const activeCitizensAsString: string = lastWeekEnd['active_citizens']
+        console.log('activeCitizensAsString:', activeCitizensAsString)
+        const activeCitizensAsNumberArray: number[] = activeCitizensAsString.split(',').map(Number)
+        console.log('activeCitizensAsNumberArray:', activeCitizensAsNumberArray)
+        
+        resolve(activeCitizensAsNumberArray)
+      }
+    })
+  })
+  return promise
+}
+
 (async () => {
+  const passportIDs = await lookupActiveCitizensFromCSV()
+  console.log('passportIDs:', passportIDs)
+
   const privateKey = process.env.CONTRACT_OWNER_PRIVATE_KEY
   if (privateKey == undefined) {
     console.error('Error: CONTRACT_OWNER_PRIVATE_KEY not found')
@@ -31,8 +73,7 @@ import { BigNumber, ethers } from "ethers"
   const nationCredContract = new ethers.Contract("0xdc5dE9960aAf60CE8C773f88E7F3cC9E8dD62130", NationCred.abi, signer)
   console.log('nationCredContract.address:', nationCredContract.address)
   console.log('nationCredContract.owner():', await nationCredContract.owner())
-
-  const passportIDs = [1,2,3]
+  
   const receipt = await nationCredContract.setActiveCitizens(passportIDs)
   console.log('receipt', receipt)
 })()
